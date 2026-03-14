707d765d68691c8574502a91dba0734ef0368c74
sthira1236-hash <sthira1236@gmail.com>
2026-03-14 14:36:43 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,21 +1,27 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
-/* Root route */
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "public")));
+
 app.get("/", (req, res) => {
-  res.send("SMS Gateway Running");
+res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
-/* Health check route */
 app.get("/status", (req, res) => {
-  res.json({ status: "online" });
+res.json({ status: "online" });
 });
 
-/* Railway port */
+app.post("/clear-queue", (req, res) => {
+res.send("Queue cleared");
+});
+
 const PORT = process.env.PORT || 3000;
 
-/* IMPORTANT: bind to 0.0.0.0 */
 app.listen(PORT, "0.0.0.0", () => {
-  console.log("Server running on port " + PORT);
+console.log("Server running on port " + PORT);
 });
